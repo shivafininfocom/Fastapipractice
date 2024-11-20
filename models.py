88f558afba01bce12d78bdd2955c8adb9dcefea7
shivafininfocom <shiva@fininfocom.com>
2024-11-20 20:06:48 +0530

    initial commit

--- models.py
+++ models.py
@@ -12,3 +12,10 @@ class StudentCreate(SQLModel):
     name: str
     age: int
     grade: str
+
+
+class NewModelGit(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    age: Optional[int]
+    grade: Optional[str]
